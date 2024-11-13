0efac163064a9fe4cd068c5ec21fa3801b578518
KozlovskyS <skozmail@gmail.com>
2024-11-13 21:49:07 +0300

    Change names variables

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int init_amount; //начальная сумма счета
+        int initAmount;  //начальная сумма счета
         int amount;      //сумма пополнения
-        int tot_amount;  //сумма счета
+        int totAmount;   //сумма счета
         int bonus = 0;   //бонусы
-/*ввод данных*/
-        init_amount = 100;
+
+        /*ввод данных*/
+        initAmount = 100;
         amount = 1100;
 
         if (amount > 1000) {             //проверка бонусных условий
             bonus = amount / 100;
             System.out.println("Поздравляем! Ваш бонус = " + bonus + " руб.");
         }
-        tot_amount = init_amount + amount + bonus;
-        System.out.println("Счет пополнен. На Вашем счету " + tot_amount + " руб.");
+        totAmount = initAmount + amount + bonus;
+        System.out.println("Счет пополнен. На Вашем счету " + totAmount + " руб.");
     }
 }
